013b4e5ebd9033d1f682e954474019d4f32be897
Flávio Campos <flaviorcampos15@gmail.com>
2025-05-18 15:48:05 -0300

    Update Main.js
    Quinta linha

--- Main.js
+++ Main.js
@@ -2,3 +2,4 @@ Exemplo de novo arquivo, linha 1
 linha 2, estranho quando eu pegue GitHib ele verificou que já existia, agora que estou salvando pelo
 segunda vez, não avisou, deve ser coisa MACOS.
 Quarta linha, vamos ver o que vai ocorrer.
+  Vamos para linha 5, editando no Github.
