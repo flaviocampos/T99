843eb7c70464d4335de6c2a2f6b629dc279a5134
flaviocampos <flaviorcampos15@gmail.com>
2025-05-18 15:49:40 -0300

    Nova linha 5 feita MACOS M1.

--- Main.js
+++ Main.js
@@ -2,3 +2,4 @@ Exemplo de novo arquivo, linha 1
 linha 2, estranho quando eu pegue GitHib ele verificou que já existia, agora que estou salvando pelo
 segunda vez, não avisou, deve ser coisa MACOS.
 Quarta linha, vamos ver o que vai ocorrer.
+Vanos ver como vai ficar isto na 5 linha
